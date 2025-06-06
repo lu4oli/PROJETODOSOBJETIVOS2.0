2d28c5f95972e3d8f47d8a86a4180a79444370ee
Luah <132931301+lu4oli@users.noreply.github.com>
2025-06-06 11:15:56 -0300

    Add files via upload

--- projeto-de-vida-3a-main/js-projeto-de-vida-codigo-base/main.js
+++ projeto-de-vida-3a-main/js-projeto-de-vida-codigo-base/main.js
@@ -15,7 +15,12 @@ for(let i=0;i < botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll (".contador");
-const tempoObjetivo1 = new Date("")
-const tempoObjetivo2 = new Date("")
-const tempoObjetivo3 = new Date("")
-const tempoObjetivo4 = new Date("")
+let tempoAtual = new Date("");
+const tempoObjetivo1 = new Date("2025-11-09T00:00:00") - tempoAtual;
+contadores[0].textContent = tempoObjetivo1;
+const tempoObjetivo2 = new Date("2026-02-04T00:00:00") - tempoAtual;
+contadores[1].textContent = tempoObjetivo2;
+const tempoObjetivo3 = new Date("2026-01-17T00:00:00") - tempoAtual;
+contadores[2].textContent = tempoObjetivo3;
+const tempoObjetivo4 = new Date("2026-03-01T00:00:00") - tempoAtual;
+contadores[3].textContent = tempoObjetivo4;
