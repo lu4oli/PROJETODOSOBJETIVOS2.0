23e15fc8d266776524ef4110971346a26b6eea34
Luah <132931301+lu4oli@users.noreply.github.com>
2025-06-24 13:49:50 -0300

    Add files via upload

--- projeto-de-vida-3a-main/js-projeto-de-vida-codigo-base/main.js
+++ projeto-de-vida-3a-main/js-projeto-de-vida-codigo-base/main.js
@@ -1,26 +1,36 @@
-const botoes = document.querySelectorAll (".botao");
-const textos = document.querySelectorAll (".aba-conteudo");
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i < botoes.length; i++){
-    botoes[i].onclick = function(){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for(let j=0;j < botoes.length; j++){
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-      botoes[i].classList.add("ativo");
-      textos[i].classList.add("ativo");
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
-    console.log();
 }
 
-const contadores = document.querySelectorAll (".contador");
-let tempoAtual = new Date("");
-const tempoObjetivo1 = new Date("2025-11-09T00:00:00") - tempoAtual;
-contadores[0].textContent = tempoObjetivo1;
-const tempoObjetivo2 = new Date("2026-02-04T00:00:00") - tempoAtual;
-contadores[1].textContent = tempoObjetivo2;
-const tempoObjetivo3 = new Date("2026-01-17T00:00:00") - tempoAtual;
-contadores[2].textContent = tempoObjetivo3;
-const tempoObjetivo4 = new Date("2026-03-01T00:00:00") - tempoAtual;
-contadores[3].textContent = tempoObjetivo4;
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-01-16T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    dias %= 365;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
